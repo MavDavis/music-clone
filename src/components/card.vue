93a42e9a3867af9d8c4bcc35d8ebe458c685c937
Mavdavis <ekwesimarvelous@gmail.com>
2022-08-30 17:06:46 -0700

    commit

--- src/components/card.vue
+++ src/components/card.vue
@@ -3,9 +3,13 @@
 <div class="p-2 w-48 card relative"  v-for="(cards,index) in songs" :key="index">
 <div class="absolute w-full h-full justify-end items-end p-8 flex opacity-0 hover:opacity-100">
     <div class="rounded-full bg-green h-10 w-10 flex items-center justify-center">
-        <i v-if="$store.state.singlePlaying" class="fas fa-play text-white text-xl" @click="$store.commit('playSingle', cards.title)" ></i>
-        <i v-else class="fas fa-pause text-white text-x1"></i>
+        <i v-if="!cards.isPlaying" class="fas fa-play text-white text-xl" @click="$store.commit('playSingle', cards.title)" ></i>
+        <i v-else class="fas fa-pause text-white text-x1" @click="$store.commit('pauseSingle', cards.title)"></i>
     </div>
+    
+</div>
+<div class="absolute w-5 h-5 justify-start items-end p-4 opacity-0 hover:opacity-100">
+    <i class="fas fa-heart text-xl text-white" :class="{ like:cards.isLiked}" @click="cards.isLiked = !cards.isLiked"></i>
 </div>
     <div class="bg-light w-full h-60 p-5 shadow-md rounded-lg ">
     <img :src="cards.image" alt="card-img" class="h-1/2 w-full shadow mb-2">
@@ -23,6 +27,9 @@ props:['songs']
 </script>
 
 <style lang="scss">
+    i.like{
+        color:#1DB954;
+    }
 // .card{
 //     transition: 0.5s;
 
--- src/store/index.js
+++ src/store/index.js
@@ -11,60 +11,89 @@ export default createStore({
           artist: "Unknown",
           url: require("../assets/audio/beats.mp3"),
           image: "https://source.unsplash.com/crs2vlkSe98/400x400",
+          isPlaying:false,
+          isLiked:false,
         },
         {
           title: " Hard Drill Instrumental  ",
           artist: "Mastermind Drake x Popsmoke",
           url: require("../assets/audio/hardDrill.mp3"),
           image: "https://source.unsplash.com/35bE_njbG9E/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: " For My Hand ",
           artist: "Burna Boy -feat. Ed Sheeran",
           url: require("../assets/audio/formyhand.mp3"),
           image: "https://source.unsplash.com/Esax9RaEl2I/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "It s Plenty",
           artist: "Burna Boy  ",
           url: require("../assets/audio/itsPlenty.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "Juice WRLD DID (Official Audio)",
           artist: "DJ Khaled - ft. Juice WRLD  ",
           url: require("../assets/audio/juicedid.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "CYMATICS Science",
           artist: "Nigel Stanford  ",
           url: require("../assets/audio/physics.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: " Mockingbird ",
           artist: "Eminem ",
           url: require("../assets/audio/mockingbird.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "Sasha Sloan",
           artist: "Older ",
           url: require("../assets/audio/older.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "Hillsong",
           artist: "Silent Night  ",
           url: require("../assets/audio/silent.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
         {
           title: "I'm a prisoner",
           artist: "Lucky Dube ",
           url: require("../assets/audio/prisoner.mp3"),
           image: "https://source.unsplash.com/j0g8taxHZa0/400x400",
+          isPlaying:false,
+          isLiked:false,
+
         },
       ],
       player: new Audio(),
@@ -73,20 +102,30 @@ export default createStore({
       duration: "00:00",
       durationLeft: "",
       playingTime:0,
-      singlePlaying:true,
+  
     };
   },
   mutations: {
     playSingle(state, n){
 let array = state.playlist;
 let song = array.find(item => item.title == n);
+let songs = array.filter(item => item.title !== n);
+songs.map(item=> item.isPlaying = false)
 state.index = (array.indexOf(song)); 
 state.isPlaying = true;
 state.currentTitle =song.title;
 state.currentArtist = song.artist;
 state.currentImageSrc = song.image;
 state.player.src = song.url;
+song.isPlaying = true
 state.player.play();
+    },
+    pauseSingle(state, n){
+      let array = state.playlist;
+      let song = array.find(item => item.title == n);
+      state.player.pause();
+      song.isPlaying = false
+      state.isPlaying = false;
     },
     played(state) {
       state.isPlaying = true;
@@ -115,7 +154,7 @@ state.player.play();
       state.isPlaying = false;
       state.player.pause();
       state.duration = state.player.duration;
-
+    
     },
     next(state) {
       if (state.index <= state.playlist.length - 2) {
